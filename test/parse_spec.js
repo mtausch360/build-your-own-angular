5db366f91ee4a313a5676d55da9ad8a980ffd1fd
Mark Tausch <tausch86@gmail.com>
2016-06-26 14:06:04 -0700

    altering precendence

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -698,6 +698,13 @@ describe('parse', function() {
     }) ).toEqual('c');
   });
 
+  //altering precedence orders
+  it('parses parentheses altering precendence order', function(){
+    expect( parse('21 * (3 - 1)')() ).toBe(42);
+    expect( parse('false && (true || true)')() ).toBe(false);
+    expect( parse('-((a % 2) === 0 ? 1 : 2)')({a:42}) ).toBe(-1);
+  });
+
 });
 
 
