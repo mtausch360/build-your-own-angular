a4bb1e453f140081cae8b652faecc6ae526a37c4
Mark Tausch <tausch86@gmail.com>
2016-06-27 17:13:40 -0700

    started angular module

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,62 @@
 function setupModuleLoader (window) {
-  var angular = window.angular = {};
+  //only want to load angular once, and any extraneous calls should not replace the original angular object
+  let angular = ensure(window, 'angular', Object);
+
+  let module = ensure(angular, 'module', ()=>{
+    let moduleStore = {};
+    return (name, requires)=>{
+      if(requires){
+        return createModule(name, requires, moduleStore);
+      } else {
+        return getModule(name, moduleStore);
+      }
+    };
+  });
+
+  /**
+   * [createModule description]
+   * @param  {[type]} name     [description]
+   * @param  {array} requires [description]
+   * @param  {obj} moduleStore [comes from main execute block]
+   * @return {obj}          [description]
+   */
+  function createModule(name, requires, moduleStore){
+    if( name === 'hasOwnProperty')
+      throw 'Invalid Module Name: ' + name;
+
+    const moduleInstance = {
+      name,
+      requires
+    };
+
+    moduleStore[name] = moduleInstance;
+
+    return moduleInstance;
+  }
+
+  /**
+   * [getModule description]
+   * @param  {[type]} name   [description]
+   * @param  {[type]} moduleStore [description]
+   * @return {[type]}        [description]
+   */
+  function getModule(name, moduleStore){
+    if( moduleStore.hasOwnProperty(name))
+      return moduleStore[name];
+    else
+      throw 'Module ' + name + ' doesn\'t exist';
+  }
+
+  /**
+   * creates a property in the given object with the given factory or returns already existing object
+   * @param  {[type]} obj     [description]
+   * @param  {[type]} name    [description]
+   * @param  {[type]} factory [description]
+   * @return {[type]}         [description]
+   */
+  function ensure(obj, name, factory){
+    return obj[name] || ( obj[name] = factory() );
+  }
 }
 
 export { setupModuleLoader };
--- test/loader_spec.js
+++ test/loader_spec.js
@@ -2,9 +2,74 @@ import {setupModuleLoader} from '../src/loader';
 
 describe('setupModuleLoader', ()=>{
 
+  beforeEach(()=>{
+    delete window.angular;
+  });
+
   it("exposes angular on the window", ()=>{
     setupModuleLoader(window);
     expect(window.angular).toBeDefined();
+  });
+
+  it('creates angular just once', ()=>{
+    setupModuleLoader(window);
+    const ng = window.angular;
+    setupModuleLoader(window);
+    expect(window.angular).toBe(ng);
+  });
+
+  //module method
+  it('exposes the angular module function', ()=>{
+    setupModuleLoader(window);
+    expect(window.angular.module).toBeDefined();
+  });
+
+  it('exposes the angular module function just once', ()=>{
+    setupModuleLoader(window);
+    const mod = window.angular.module;
+    setupModuleLoader(window);
+    expect(window.angular.module).toBe(mod);
+  });
+
+  describe('modules', ()=>{
+
+    beforeEach(()=>{
+      setupModuleLoader(window);
+    });
+
+    it('allows registering a module', ()=>{
+      let myModule = window.angular.module('myModule', []);
+      expect(myModule).toBeDefined();
+      expect(myModule.name).toEqual('myModule');
+    });
+
+    it('replaces a module of the same name again', ()=>{
+      let myModule = window.angular.module('myModule', []);
+      let myNewModule = window.angular.module('myModule', []);
+      expect(myNewModule).not.toBe(myModule);
+    });
+
+    it('attaches the requires array to the registered module', ()=>{
+      let requires = [];
+      let myModule = window.angular.module('myModule', requires);
+      expect(myModule.requires).toBe(requires);
+    });
+
+    it('allows getting a module', ()=>{
+      let myMod = window.angular.module('myMod', []);
+      let gotModule = window.angular.module('myMod');
+      expect(myMod).toBe(gotModule);
+    });
+
+    it('throws when getting nonexistent module', ()=>{
+      expect(()=>window.angular.module('myMod')).toThrow();
+    });
+
+    it('does not allow a module called hasOwnProperty', ()=>{
+      expect(()=>window.angular.module('hasOwnProperty', [])).toThrow();
+    });
+    
+
   });
 
 });
